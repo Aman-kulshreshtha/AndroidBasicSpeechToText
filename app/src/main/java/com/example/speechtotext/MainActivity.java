d729b3cc12020124740b2c59f7bee7440576ba26
Aman Kulshreshtha <aman21102000@gmail.com>
2020-08-17 06:46:14 +0530

    Added comments to MainActivity.java

--- app/src/main/java/com/example/speechtotext/MainActivity.java
+++ app/src/main/java/com/example/speechtotext/MainActivity.java
@@ -21,21 +21,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        txt = (TextView)findViewById(R.id.txtV);
+        txt = (TextView)findViewById(R.id.txtV);  //identified the textView by it's id so that we can grab it to put our answer text in it
 
 
     }
 
 
 
-    public void getInput(View view){
-        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+    public void getInput(View view){   //This method is called whenever the user presses the mic button
+        /*Purpose of this method is to create something that first checks whether speech recognition is available in the device or not
+         then it listens to the speech of the user ,recognzes the language and then stores the result*/
+
+
+
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);              //intent is used to perform the task of speech recognition
+
+
+        //we tell the intent to use the free form language model this is required by the ACTION_RECOGNIZE_SPEECH
         intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+
+        //then we say that in case of no connectivity with internet use default language of the local device
         intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
 
 
         if(intent.resolveActivity(getPackageManager()) != null){
-            startActivityForResult(intent,1);
+            startActivityForResult(intent,1);//if speech recognition is supported start listening what user says onActivityResult method is called here
         }else
             Toast.makeText(this,"Your Device doesn't supports speech input",Toast.LENGTH_SHORT).show();
 
@@ -49,10 +59,10 @@ public class MainActivity extends AppCompatActivity {
 
         switch(requestCode){
             case 1:
-                if(resultCode == RESULT_OK && data != null){
-                    ArrayList<String> result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                if(resultCode == RESULT_OK && data != null){   // if the intent successfully listened and stored the data
+                    ArrayList<String> result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS); //store result in an array list
 
-                    txt.setText(result.get(0));
+                    txt.setText(result.get(0));//display the result in textView
                 }
         }
     }
